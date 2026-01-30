a1756a851b5a3c3ffe2d20878dbaddcdf82f3aa4
tracer-NEMU2020 <tjuics2020@163.com>
2026-01-30 17:48:08 +0800

    >  compile NEMU 3024244467 root Linux kyler 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:48:06 up 2 days,  5:49,  1 user,  load average: 0.41, 0.54, 0.59 2c6d9086e0da3b6f1d4f532124f52b023c5d599a

--- nemu/src/cpu/exec/data-mov/cmov.c
+++ nemu/src/cpu/exec/data-mov/cmov.c
@@ -2,10 +2,22 @@
 
 #define DATA_BYTE 2
 #include "cmov-template.h"
+#undef instr
+#include "cmovne-template.h"
+#undef instr
+#include "cmovns-template.h"
+#undef instr
 #undef DATA_BYTE
 
 #define DATA_BYTE 4
 #include "cmov-template.h"
+#undef instr
+#include "cmovne-template.h"
+#undef instr
+#include "cmovns-template.h"
+#undef instr
 #undef DATA_BYTE
 
 make_helper_v(cmovs_rm2r)
+make_helper_v(cmovne_rm2r)
+make_helper_v(cmovns_rm2r)
--- nemu/src/cpu/exec/data-mov/cmov.h
+++ nemu/src/cpu/exec/data-mov/cmov.h
@@ -4,5 +4,11 @@
 make_helper(cmovs_rm2r_w);
 make_helper(cmovs_rm2r_l);
 make_helper(cmovs_rm2r_v);
+make_helper(cmovne_rm2r_w);
+make_helper(cmovne_rm2r_l);
+make_helper(cmovne_rm2r_v);
+make_helper(cmovns_rm2r_w);
+make_helper(cmovns_rm2r_l);
+make_helper(cmovns_rm2r_v);
 
 #endif
